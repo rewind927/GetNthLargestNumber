88a2161090819d767017a67e78e5b5094dc719dd
RyanWang <ryanwang@KKBOXdeMacBook-Pro-2.local>
2014-09-12 11:32:44 +0800

    using swap function instead of list control funciton

--- GetNthLargestNumber.py
+++ GetNthLargestNumber.py
@@ -5,6 +5,11 @@ def printNum():
     for num in array:
         print num
 
+def swap(array,x,y):
+    tmp = array[x]
+    array[x] = array[y]
+    array[y] = tmp
+
 number = raw_input("Input number: ")
 if number.isdigit():
     number = int(number)
@@ -12,24 +17,24 @@ else:
     print("not digital number, break this loop!")
     quit()
 
-array = [0]
+array = [0]*(number+1)
 #print "========== input data =========="
 f = open(sys.argv[1], 'r')
 for data in f:
-    data = data.strip()
+    data = int(data.strip())
 #    print data
-    i = 0
-    while i < len(array):
-        if int(data) > int(array[i]):
-            if i == 0:
-                array = [data] + array[:number-1]
-            elif i == number-1:
-                array = array[:number-1]+[data]
-            else :
-                array = array[:i] + [data] + array[i:number-1]
-            break
-        i+=1
+    if data > array[number]:
+        array[number] = data
+        i = number
+        while i > 0:
+            if array[i] > array[i-1]:
+               swap(array,i,i-1)
+            else:
+                break
+            i-=1
 f.close()
+
+array.pop()
 
 printNum()
 
